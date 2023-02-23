5119d1297dd39ee9823e0eaf68e372501c7350f6
Rajesh Kumar <rajesh.kumar11@kyndryl.com>
2023-02-23 20:49:04 +0530

    fix: delete node in binary tree added

--- binarytree/btree/btree.go
+++ binarytree/btree/btree.go
@@ -4,15 +4,15 @@ import "fmt"
 
 // Node ...
 type Node struct {
-	data  int
+	value int
 	left  *Node
 	right *Node
 }
 
 // CreateNode ...
-func CreateNode(data int) *Node {
+func CreateNode(value int) *Node {
 	return &Node{
-		data:  data,
+		value: value,
 		left:  nil,
 		right: nil,
 	}
@@ -25,9 +25,9 @@ func InsertNode(root *Node, data int) *Node {
 		return newNode
 	}
 
-	if data < root.data {
+	if data < root.value {
 		root.left = InsertNode(root.left, data)
-	} else if data > root.data {
+	} else if data > root.value {
 		root.right = InsertNode(root.right, data)
 	}
 
@@ -38,7 +38,45 @@ func InsertNode(root *Node, data int) *Node {
 func TraverseInorder(root *Node) {
 	if root != nil {
 		TraverseInorder(root.left)
-		fmt.Printf("%d ", root.data)
+		fmt.Printf("%d ", root.value)
 		TraverseInorder(root.right)
 	}
+}
+
+// Delete nodes...
+func DeleteNode(root *Node, value int) *Node {
+	if root == nil {
+		return nil
+	}
+
+	// Search for the node to be deleted
+	if value < root.value {
+		root.left = DeleteNode(root.left, value)
+		return root
+	} else if value > root.value {
+		root.right = DeleteNode(root.right, value)
+		return root
+	}
+
+	// If the node has no children
+	if root.left == nil && root.right == nil {
+		return nil
+	}
+
+	// If the node has one child
+	if root.left == nil {
+		return root.right
+	} else if root.right == nil {
+		return root.left
+	}
+
+	// If the node has two children
+	minRight := root.right
+	for minRight.left != nil {
+		minRight = minRight.left
+	}
+	root.value = minRight.value
+	root.right = DeleteNode(root.right, minRight.value)
+
+	return root
 }
